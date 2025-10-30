e70860dbf87542f31e0494f6080903a219271c42
ElenaTheAxolotl <elenavasquez0130@gmail.com>
2025-10-30 15:39:32 -0400

    ex 2.6 & 2.7

--- index.py
+++ index.py
@@ -1,2 +1,11 @@
 # write code goofy
-username = "goofy"
+# username = "goofy"
+# print("Hi", username)
+
+#color = "blue
+#print(color)
+
+animal = "cat"
+print("I have a", animal)
+animal = "dog"
+print("Now I have a", animal)
